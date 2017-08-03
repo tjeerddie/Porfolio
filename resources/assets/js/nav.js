8e9d697f34e1db8fdd70fee16b7423445fea533d
tjeerddie <tjeerdverschragen@hotmail.com>
2017-08-03 11:26:13 +0200

    package.json: added scripts to run gulp. index.html: added lang to html tag. changed the title to my name. moved the fonts to load after the css. removed the br tags that divided the text and instead divided them with p tags. nav.js: changed var's to const and let where possible. _mixin.scss: changed the transition mixin to giving all the arguments, this makes it possible to add transition for multiple types. *.scss: changed the transition include to give all the arguments needed. portfolio.png: img to use as the icon.

--- resources/assets/js/nav.js
+++ resources/assets/js/nav.js
@@ -1,18 +1,18 @@
-var nav = (function (document) {
-  var getClassElements = function (name) {
+const nav = (function (document) {
+  const getClassElements = function (name) {
     return document.getElementsByClassName(name) ? document.getElementsByClassName(name) : "can't find the element";
   }
 
-  var nav = getClassElements('nav__list')[0];
-  var navItems = Array.prototype.slice.call(nav.children);
-  var body = document.getElementsByTagName("BODY")[0];
-  var aside = getClassElements('developer')[0];
-  var activeSection = getClassElements('about')[0];
-  var background1 = getClassElements('background-1')[0];
-  var background3 = getClassElements('background-3')[0];
-  var phoneSize = 768;
+  let nav = getClassElements('nav__list')[0];
+  let navItems = Array.prototype.slice.call(nav.children);
+  let body = document.getElementsByTagName("BODY")[0];
+  let aside = getClassElements('developer')[0];
+  let activeSection = getClassElements('about')[0];
+  let background1 = getClassElements('background-1')[0];
+  let background3 = getClassElements('background-3')[0];
+  let phoneSize = 768;
 
-  var init = function () {
+  const init = function () {
     navItems.forEach(function(item) {
       item.addEventListener('click', selected);
     }, this);
@@ -28,19 +28,19 @@ var nav = (function (document) {
     });
   }
 
-  var getActive = function () {
+  const getActive = function () {
     return getClassElements('nav__list-item--active')[0];
   }
 
-  var getSection = function (name) {
+  const getSection = function (name) {
     return getClassElements(name)[0];
   }
 
-  var selected = function (e) {
-    var active = getActive();
+  const selected = function (e) {
+    let active = getActive();
     active.classList.toggle('nav__list-item--active');
     
-    var item = e.target.nodeName == "I" ? e.target.parentElement : e.target;
+    let item = e.target.nodeName == "I" ? e.target.parentElement : e.target;
     item.classList.toggle('nav__list-item--active');
 
     activeSection.classList.toggle('ghost');
